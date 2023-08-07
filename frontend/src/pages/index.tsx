90efe05d3249a01056a4bec98b6f3084090bcc91
jacoboArcila <wagner.arcila.jacobo@gmail.com>
2023-08-07 14:00:37 -0500

    test

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -6,7 +6,7 @@ export default function Home() {
 	return (
 		<main>
 			<SuggestionsStyle>
-				<h1 className="suggestion_title">Nuestra sugerencia</h1>
+				<h1 className="suggestion_title">Nuestrasssssssssss sugerencia</h1>
 				<p className="suggestion_p">
 					De clásicos atemporales a gemas contemporáneas, estas recomendaciones te llevarán a un viaje inolvidable en el
 					fascinante mundo del entretenimiento
