d195b3e17f3f8e38062367877d56ad40abcd9a47
Adrian Moncada <adrianmoncada@VirtualBox-029584dc.localdomain>
2023-08-06 22:54:31 -0300

    testing ci

--- backend/api-movie/src/main/java/com/dh/movie/controller/MovieController.java
+++ backend/api-movie/src/main/java/com/dh/movie/controller/MovieController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/v1/movie")
+@RequestMapping("/api/v1/movies")
 @AllArgsConstructor
 public class MovieController {
 
