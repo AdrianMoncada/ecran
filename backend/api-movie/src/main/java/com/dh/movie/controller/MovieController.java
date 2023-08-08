02592cbeef484ec5ff034eda7551cacfc8d2a233
Tomas Alegria Caceres <tomialegriacaceres@gmail.com>
2023-08-07 23:37:03 -0300

    back to 8080

--- backend/api-movie/src/main/java/com/dh/movie/controller/MovieController.java
+++ backend/api-movie/src/main/java/com/dh/movie/controller/MovieController.java
@@ -6,9 +6,7 @@ import com.dh.movie.service.MovieService;
 import lombok.AllArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-
 import javax.validation.Valid;
-import javax.websocket.server.PathParam;
 import java.util.List;
 
 @RestController
