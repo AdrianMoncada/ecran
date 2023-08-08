da07b9236845536dd8329f7245c74214af0de095
Tomas Alegria Caceres <tomialegriacaceres@gmail.com>
2023-08-07 23:33:22 -0300

    temporalmente desabilitado eureka y config

--- backend/api-movie/src/main/java/com/dh/movie/ApiMovieApplication.java
+++ backend/api-movie/src/main/java/com/dh/movie/ApiMovieApplication.java
@@ -6,7 +6,6 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 /*import org.springframework.cloud.openfeign.EnableFeignClients;*/
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.Bean;
 /*import org.springframework.cloud.client.discovery.EnableDiscoveryClient;*/
 
@@ -14,8 +13,7 @@ import java.util.ArrayList;
 
 
 @SpringBootApplication
-
-@EnableDiscoveryClient
+/*@EnableDiscoveryClient*/
 /*@EnableFeignClients*/
 public class ApiMovieApplication {
 
