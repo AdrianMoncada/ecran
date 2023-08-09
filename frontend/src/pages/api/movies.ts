01e10011da2eac54ae6332dbe76f78a84909e4e5
VilattaRomina <vilattaromina@gmail.com>
2023-08-08 21:22:06 -0300

    apis

---
+++ frontend/src/pages/api/movies.ts
@@ -0,0 +1,6 @@
+import movies from "@/assets/carousel.json";
+import { NextApiRequest, NextApiResponse } from "next";
+
+export default function handler(req: NextApiRequest, res: NextApiResponse) {
+	res.status(200).json(movies);
+}
