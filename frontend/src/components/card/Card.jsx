2fd27987b44388c8ac95f64b312deea3c79f8190
diana ramos <dianamil1941@gmail.com>
2023-08-10 11:16:44 -0500

    se creo la pagina de datalle, se aplicaron estilos y directo se consumio el json estatico

--- frontend/src/components/card/Card.jsx
+++ frontend/src/components/card/Card.jsx
@@ -2,8 +2,15 @@ import React from "react";
 import { Container, CardHeader } from "./Card.styles";
 /* import Image from "next/image"; */
 import { BsCircleFill } from "react-icons/bs";
+import Link from "next/link";
+import { useRouter } from "next/router";
 
 const Card = ({ movie }) => {
+	const router = useRouter();
+
+	const handleClick = () => {
+		router.push(`/movies/${movie.id}`);
+	};
 	const textoOriginal = movie.descripcion;
 	const limiteCaracteres = 70;
 	const textoCortado =
@@ -22,7 +29,9 @@ const Card = ({ movie }) => {
 				</CardHeader>
 				<h1 className="title">{movie.title}</h1>
 				<p className="description">{textoCortado}</p>
-				<a className="button">Ver más</a>
+				<p onClick={handleClick} className="button">
+					Ver más
+				</p>
 			</Container>
 		</div>
 	);
