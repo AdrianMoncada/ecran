74aedb67861822ed994019a9bdfdbecb895f785a
zoezn <zxejimenez@gmail.com>
2023-08-12 16:01:42 -0300

    chore: working swagger ui read only

--- backend/api-series/src/main/java/com/dh/series/config/SwaggerConfig.java
+++ backend/api-series/src/main/java/com/dh/series/config/SwaggerConfig.java
@@ -1,57 +1,33 @@
 package com.dh.series.config;
 
-import com.dh.series.controller.SerieController;
+//import io.swagger.v3.oas.annotations.ExternalDocumentation;
+
+import io.swagger.v3.oas.models.ExternalDocumentation;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+import org.springdoc.core.models.GroupedOpenApi;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
-import springfox.documentation.builders.PathSelectors;
-import springfox.documentation.builders.RequestHandlerSelectors;
-import springfox.documentation.service.ApiInfo;
-import springfox.documentation.service.Contact;
-import springfox.documentation.spi.DocumentationType;
-import springfox.documentation.spring.web.plugins.Docket;
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
-import springfox.documentation.swagger2.annotations.EnableSwagger2WebMvc;
-
-import java.util.Collections;
 
 @Configuration
-@EnableSwagger2
-@EnableWebMvc
-@EnableSwagger2WebMvc
-//@EnableSw
 public class SwaggerConfig {
-//    @Bean
-//    public Docket api() {
-//        return new Docket(DocumentationType.SWAGGER_2)
-//                .select()
-//                .apis(RequestHandlerSelectors.any())
-//                .paths(PathSelectors.any())
-//                .build();
-//    }
-//    @Bean
-    public Docket apiDocket() {
-        return new Docket(DocumentationType.SWAGGER_2)
-                .select()
-                .apis(RequestHandlerSelectors.basePackage("com.dh.series.controller"))
-//                .apis(RequestHandlerSelectors.withClassAnnotation(SerieController.class))
-//                .paths(PathSelectors.s("/api/*"))
-                .paths(PathSelectors.regex("/api/.*"))
-                .build()
-                .apiInfo(getApiInfo())
-                ;
+    public static final String SWAGGER2URL = "/api/v1/docs";
+
+    @Bean
+    public GroupedOpenApi publicApi() {
+        return GroupedOpenApi.builder()
+
+                .group("series-controller")
+                .pathsToMatch("/**")
+                .build();
     }
 
-    private ApiInfo getApiInfo() {
-        return new ApiInfo(
-                "Order Service API",
-                "Order Service API Description",
-                "1.0",
-                "http://codmind.com/terms",
-                new Contact("Codmind", "https://codmind.com", "apis@codmind.com"),
-                "LICENSE",
-                "LICENSE URL",
-                Collections.emptyList()
-        );
+    @Bean
+    public OpenAPI springShopOpenAPI() {
+        return new OpenAPI()
+                .info(new Info().title("Series MS")
+                        .description("Microservicio de series para el proyecto integrador 2")
+                        .version("v1.0.0"));
     }
 }
--- backend/api-series/src/main/java/com/dh/series/controller/SerieController.java
+++ backend/api-series/src/main/java/com/dh/series/controller/SerieController.java
@@ -3,8 +3,8 @@ package com.dh.series.controller;
 import com.dh.series.model.Serie;
 import com.dh.series.model.dto.SerieDTO;
 import com.dh.series.service.SeasonServiceImpl;
-import io.swagger.annotations.Api;
-import io.swagger.annotations.ApiOperation;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.AllArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -15,36 +15,36 @@ import java.util.Optional;
 @RestController
 @RequestMapping("/api/v1/series")
 @AllArgsConstructor
-@Api(value = "Series MS", description = "Endpoints del microservicio de series")
+@Tag(name = "Series MS", description = "Endpoints del microservicio de series")
 public class SerieController {
     private final SeasonServiceImpl service;
 
     @PostMapping()
-    @ApiOperation("Guardar una serie")
+    @Operation(description = "Guardar una serie")
     private Serie postSeries(@RequestBody SerieDTO serie) throws Exception {
         return service.save(serie);
     }
 
     @GetMapping("/{id}")
-    @ApiOperation("Traer una serie por id")
+    @Operation(description = "Traer una serie por id")
     private ResponseEntity<Optional<Serie>> getById(@PathVariable String id) throws Exception {
         return ResponseEntity.ok(service.getById(id));
     }
 
     @GetMapping()
-    @ApiOperation("Traer todas las series")
+    @Operation(description = "Traer todas las series")
     private ResponseEntity<List<Serie>> getAllSeries() {
         return ResponseEntity.ok(service.getAll());
     }
 
     @DeleteMapping("/{id}")
-    @ApiOperation("Borrar una serie por id")
+    @Operation(description = "Borrar una serie por id")
     private ResponseEntity<String> deleteById(@PathVariable String id)  throws Exception{
         return ResponseEntity.ok(service.deleteById(id));
     }
 
     @PutMapping()
-    @ApiOperation("Modificar una serie por id")
+    @Operation(description = "Modificar una serie por id")
     private ResponseEntity<Serie> updateById(@RequestBody Serie serie)  throws Exception{
         return ResponseEntity.ok(service.updateById(serie));
     }
