aeac7ad0d88ca9ccb49f31ed3873f40f789997c8
Adrian Moncada <adrianmoncada@VirtualBox-029584dc.localdomain>
2023-08-16 17:04:35 -0300

    agrego dependencia eureka client a ms series y anotacion

--- backend/api-series/src/main/java/com/dh/series/SeriesApplication.java
+++ backend/api-series/src/main/java/com/dh/series/SeriesApplication.java
@@ -2,10 +2,12 @@ package com.dh.series;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @SpringBootApplication
 @EnableWebMvc
+@EnableDiscoveryClient
 public class SeriesApplication {
 
 	public static void main(String[] args) {
