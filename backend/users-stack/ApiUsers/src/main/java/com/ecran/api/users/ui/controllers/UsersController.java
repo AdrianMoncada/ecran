b4e0604a3880b15899f40d432c91a7a60c414b7b
Adrian Moncada <adrianmoncada@VirtualBox-029584dc.localdomain>
2023-08-15 23:46:53 -0300

    Modificando config server

--- backend/users-stack/ApiUsers/src/main/java/com/ecran/api/users/ui/controllers/UsersController.java
+++ backend/users-stack/ApiUsers/src/main/java/com/ecran/api/users/ui/controllers/UsersController.java
@@ -34,7 +34,8 @@ public class UsersController {
 	@GetMapping("/status/check")
 	public String status()
 	{
-		return "Working on port " + env.getProperty("local.server.port");
+		return "Working on port " + env.getProperty("local.server.port") +
+				", with token = " + env.getProperty("token.secret");
 	}
  
 	@PostMapping(
