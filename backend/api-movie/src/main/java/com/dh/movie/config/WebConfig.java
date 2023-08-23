f030da36d4b05c73fcfa95578dcd29c7dc58f984
Tomas Alegria Caceres <tomialegriacaceres@gmail.com>
2023-08-23 17:32:41 +0000

    Update backend/api-movie/src/main/java/com/dh/movie/config/WebConfig.java

--- backend/api-movie/src/main/java/com/dh/movie/config/WebConfig.java
+++ backend/api-movie/src/main/java/com/dh/movie/config/WebConfig.java
@@ -1,4 +1,4 @@
-package com.dh.movie.config;
+/*package com.dh.movie.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
@@ -14,3 +14,4 @@ public class WebConfig implements WebMvcConfigurer {
                 .allowedHeaders("Access-Control-Allow-Origin", "*");
     }
 }
+*/
