86d2b0947c8266b9f6b44b19b3760cf373495e9e
Adrian Moncada <adrianmoncada@VirtualBox-029584dc.localdomain>
2023-08-22 21:07:52 -0300

    Cambios en api users feign

--- backend/users-stack/ApiUsers/src/main/java/com/ecran/api/users/data/MoviesServiceClient.java
+++ backend/users-stack/ApiUsers/src/main/java/com/ecran/api/users/data/MoviesServiceClient.java
@@ -1,14 +1,24 @@
 package com.ecran.api.users.data;
 
 import com.ecran.api.users.ui.model.MoviesResponseModel;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @FeignClient(name="movies")
 public interface MoviesServiceClient {
     @GetMapping("/users/{id}/movies")
+    @CircuitBreaker(name="movies", fallbackMethod ="getMoviesFallback")
     public List<MoviesResponseModel> getAlbums(@PathVariable String id);
+
+    default List<MoviesResponseModel> getMoviesFallback(String id, Throwable exception){
+        System.out.println(id);
+        System.out.println(exception.getMessage());
+        return new ArrayList<>();
+    }
 }
+
