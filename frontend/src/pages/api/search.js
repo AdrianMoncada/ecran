e76c030c5d8842686f0524cae83773ead97d0603
VilattaRomina <vilattaromina@gmail.com>
2023-08-24 16:20:31 -0300

    cambio ruta

--- frontend/src/pages/api/search.js
+++ frontend/src/pages/api/search.js
@@ -1,23 +1,13 @@
 export async function fetchMoviesByTitle(query) {
 	try {
-		const response = await fetch(`http://3.95.255.94:8080/api/v1/movies/search?title=${query}`);
-		console.log(response);
+		const response = await fetch(
+			`https://83n5sz9zvl.execute-api.us-east-1.amazonaws.com/api/v1/movies/search?title=${query}`,
+		);
 		const data = await response.json();
+		console.log(data);
 		return data;
 	} catch (error) {
 		console.error("Error fetching movie data:", error);
 		throw error;
 	}
 }
-
-// export async function fetchMoviesByTitle(query) {
-// 	try {
-// 		const response = await fetch(`https://rickandmortyapi.com/api/character/?name=${query}`);
-// 		console.log(response);
-// 		const data = await response.json();
-// 		return data;
-// 	} catch (error) {
-// 		console.error("Error fetching movie data:", error);
-// 		throw error;
-// 	}
-// }
