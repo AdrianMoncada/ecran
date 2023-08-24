22ae2339cc05632e1b964a74ee5499eed7dfb152
VilattaRomina <vilattaromina@gmail.com>
2023-08-24 16:30:38 -0300

    cambio url

--- frontend/src/pages/api/search.js
+++ frontend/src/pages/api/search.js
@@ -1,6 +1,6 @@
 export async function fetchMoviesByTitle(query) {
 	try {
-		const response = await fetch(`http://3.95.255.94:8080/api/v1/movies/search?title=${query}`);
+		const response = await fetch(`https://83n5sz9zvl.execute-api.us-east-1.amazonaws.com/api/v1/movies/search?title=${query}`);
 		console.log(response);
 		const data = await response.json();
 		return data;
