05285b4086d43171da799b0a4a1346f4e907224a
Adrian Moncada <adrianmoncada@VirtualBox-029584dc.localdomain>
2023-08-30 11:42:52 -0300

    agregando dockerfiles de microservicios

--- backend/users-stack/ApiGateway/src/main/java/com/ecran/ApiGateway/ApiGatewayApplication.java
+++ backend/users-stack/ApiGateway/src/main/java/com/ecran/ApiGateway/ApiGatewayApplication.java
@@ -2,6 +2,9 @@ package com.ecran.ApiGateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import reactor.netty.http.client.HttpClient;
+import io.netty.resolver.DefaultAddressResolverGroup;
 
 @SpringBootApplication
 public class ApiGatewayApplication {
@@ -9,5 +12,10 @@ public class ApiGatewayApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(ApiGatewayApplication.class, args);
 	}
+	
+	@Bean
+	public HttpClient httpClient() {
+    return HttpClient.create().resolver(DefaultAddressResolverGroup.INSTANCE);
+}
 
 }
