ed92c3843fe06155c65c8e2e347bca5ebd9b2aa9
Adrian Moncada <adrianmoncada@VirtualBox-029584dc.localdomain>
2023-08-30 21:58:53 -0300

    actualizando aplication properties de ms users

--- backend/users-stack/ApiGateway/src/main/java/com/ecran/ApiGateway/ApiGatewayApplication.java
+++ backend/users-stack/ApiGateway/src/main/java/com/ecran/ApiGateway/ApiGatewayApplication.java
@@ -2,9 +2,9 @@ package com.ecran.ApiGateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import reactor.netty.http.client.HttpClient;
-import io.netty.resolver.DefaultAddressResolverGroup;
+//import org.springframework.context.annotation.Bean;
+//import reactor.netty.http.client.HttpClient;
+//import io.netty.resolver.DefaultAddressResolverGroup;
 
 @SpringBootApplication
 public class ApiGatewayApplication {
@@ -13,9 +13,9 @@ public class ApiGatewayApplication {
 		SpringApplication.run(ApiGatewayApplication.class, args);
 	}
 	
-	@Bean
-	public HttpClient httpClient() {
-    return HttpClient.create().resolver(DefaultAddressResolverGroup.INSTANCE);
-}
+//	@Bean
+//	public HttpClient httpClient() {
+//    return HttpClient.create().resolver(DefaultAddressResolverGroup.INSTANCE);
+//}
 
 }
