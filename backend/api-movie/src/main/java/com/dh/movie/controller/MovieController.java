d23f32e63315d3a5889a0a4332c40771bf997339
Tomas Alegria Caceres <tomialegriacaceres@gmail.com>
2023-09-07 17:06:33 +0000

    Update backend/api-movie/src/main/java/com/dh/movie/controller/MovieController.java

--- backend/api-movie/src/main/java/com/dh/movie/controller/MovieController.java
+++ backend/api-movie/src/main/java/com/dh/movie/controller/MovieController.java
@@ -74,6 +74,8 @@ public class MovieController {
 
     @PostMapping("/{id}/addscore")
     ResponseEntity<String> addValoration(@PathVariable String id, @RequestBody UserValorationDTO uvDTO) {
+        System.out.println("Peticion entrante");
+        System.out.println(uvDTO);
         return new ResponseEntity<>(movieService.addValoration(id, uvDTO), HttpStatus.CREATED);
     }
 
