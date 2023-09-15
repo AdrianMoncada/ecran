1418a6660e845ee07f4e076c885e9d95f9b3444e
zoezn <zxejimenez@gmail.com>
2023-09-14 21:00:11 -0300

    fixing users/confirm ???

--- backend/users-stack/ApiUsers/src/main/java/com/ecran/api/users/security/WebSecurity.java
+++ backend/users-stack/ApiUsers/src/main/java/com/ecran/api/users/security/WebSecurity.java
@@ -24,69 +24,70 @@ import static org.springframework.security.config.Customizer.withDefaults;
 
 @Configuration
 @EnableWebSecurity
-public class WebSecurity{
-	private Environment environment;
-	private UsersService usersService;
-	private BCryptPasswordEncoder bCryptPasswordEncoder;
+public class WebSecurity {
+    private Environment environment;
+    private UsersService usersService;
+    private BCryptPasswordEncoder bCryptPasswordEncoder;
 
-	public WebSecurity(UsersService usersService, BCryptPasswordEncoder bCryptPasswordEncoder, Environment environment) {
-		this.usersService = usersService;
-		this.bCryptPasswordEncoder = bCryptPasswordEncoder;
-		this.environment=environment;
-	}
-    
+    public WebSecurity(UsersService usersService, BCryptPasswordEncoder bCryptPasswordEncoder, Environment environment) {
+        this.usersService = usersService;
+        this.bCryptPasswordEncoder = bCryptPasswordEncoder;
+        this.environment = environment;
+    }
+
     @Bean
     protected SecurityFilterChain configure(HttpSecurity http) throws Exception {
 //		http.cors(cors -> cors.disable());
 
-    	// Configure AuthenticationManagerBuilder
-    	AuthenticationManagerBuilder authenticationManagerBuilder =
-    			http.getSharedObject(AuthenticationManagerBuilder.class);
+        // Configure AuthenticationManagerBuilder
+        AuthenticationManagerBuilder authenticationManagerBuilder =
+                http.getSharedObject(AuthenticationManagerBuilder.class);
 
-    	authenticationManagerBuilder.userDetailsService(usersService)
-    	.passwordEncoder(bCryptPasswordEncoder);
+        authenticationManagerBuilder.userDetailsService(usersService)
+                .passwordEncoder(bCryptPasswordEncoder);
 
-    	AuthenticationManager authenticationManager = authenticationManagerBuilder.build();
+        AuthenticationManager authenticationManager = authenticationManagerBuilder.build();
 
-		AuthenticationFilter authenticationFilter = new AuthenticationFilter(authenticationManager, usersService, environment);
-		authenticationFilter.setFilterProcessesUrl(environment.getProperty("login.url.path"));
+        AuthenticationFilter authenticationFilter = new AuthenticationFilter(authenticationManager, usersService, environment);
+        authenticationFilter.setFilterProcessesUrl(environment.getProperty("login.url.path"));
 
 
-    	http.csrf((csrf) -> csrf.disable());
-		http.cors(withDefaults());
-    	http.authorizeHttpRequests((authz) -> authz
-        .requestMatchers(HttpMethod.POST, "/users/*").permitAll()
-								.requestMatchers(HttpMethod.POST, "/users/*/watchlist").permitAll()
-								.requestMatchers(HttpMethod.POST, "/users/*/image").permitAll()
-								.requestMatchers(HttpMethod.POST, "/users/*/comments").permitAll()
-								.requestMatchers(HttpMethod.POST, "/users/*/addrating").permitAll()
-								.requestMatchers(HttpMethod.PATCH, "/users/*/changepassword").permitAll()
-						.requestMatchers(HttpMethod.GET, "/users/**").permitAll()
-						.requestMatchers(HttpMethod.GET, "/actuator/**").permitAll()
-				.requestMatchers(HttpMethod.GET, "/users/status/check").authenticated()
+        http.csrf((csrf) -> csrf.disable());
+        http.cors(withDefaults());
+        http.authorizeHttpRequests((authz) -> authz
+                                .requestMatchers(HttpMethod.POST, "/users/*").permitAll()
+                                .requestMatchers(HttpMethod.POST, "/users/*/watchlist").permitAll()
+                                .requestMatchers(HttpMethod.POST, "/users/*/image").permitAll()
+                                .requestMatchers(HttpMethod.POST, "/users/*/comments").permitAll()
+                                .requestMatchers(HttpMethod.POST, "/users/*/addrating").permitAll()
+                                .requestMatchers(HttpMethod.PATCH, "/users/*/changepassword").permitAll()
+                                .requestMatchers(HttpMethod.GET, "/users/**").permitAll()
+                                .requestMatchers(HttpMethod.GET, "/users/*").permitAll()
+                                .requestMatchers(HttpMethod.GET, "/actuator/**").permitAll()
+                                .requestMatchers(HttpMethod.GET, "/users/status/check").authenticated()
 //        .requestMatchers(new AntPathRequestMatcher("/users/*/watchlist")).permitAll()
-				)
-				.addFilter(authenticationFilter)
-				.authenticationManager(authenticationManager)
-        .sessionManagement((session) -> session
-                .sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+                )
+                .addFilter(authenticationFilter)
+                .authenticationManager(authenticationManager)
+                .sessionManagement((session) -> session
+                        .sessionCreationPolicy(SessionCreationPolicy.STATELESS));
 
 
-    	http.headers((headers) -> headers.frameOptions((frameOptions) -> frameOptions.sameOrigin()));
+        http.headers((headers) -> headers.frameOptions((frameOptions) -> frameOptions.sameOrigin()));
 
         return http.build();
     }
 
-	@Bean
-	CorsConfigurationSource corsConfigurationSource() {
-		CorsConfiguration configuration = new CorsConfiguration();
-		configuration.setAllowedOrigins(Arrays.asList("*"));
-		configuration.setAllowedMethods(Arrays.asList("*"));
-		configuration.setAllowedHeaders(Arrays.asList("*"));
-		configuration.setExposedHeaders(Arrays.asList("*"));
-		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-		source.registerCorsConfiguration("/**", configuration);
-		return source;
-	}
+    @Bean
+    CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOrigins(Arrays.asList("*"));
+        configuration.setAllowedMethods(Arrays.asList("*"));
+        configuration.setAllowedHeaders(Arrays.asList("*"));
+        configuration.setExposedHeaders(Arrays.asList("*"));
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
+    }
 
 }
